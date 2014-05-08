54fc9752447ae73ca5d5615d122656fec3e97295
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-08 12:54:31 -0400

    stuff logic into defs

--- scratch.py
+++ scratch.py
@@ -3,32 +3,36 @@ import requests
 from pykml import parser
 import re
 
-def getCoordsFromRoot(root):
+def cleanCoordinates(coordsRaw):
+
+    # Clean up padding whitespace
+    c = re.sub('\s*', '', str(coordsRaw))
+    coords = c.split(',')
+
+    # Remove leading zeroes from negative values, since float() fails on that format.
+    coords = [re.sub('^0-', '-', c) for c in coords]
+    # Convert all numbers to floats
+    coords = [float(c) for c in coords]
+    return coords
+
+
+def extractCoordinatesFromKML(rawKML):
+    root = parser.fromstring(rawKML)
     coords = list()
     for c in root.Document.Placemark.MultiGeometry.getchildren():
-        coords.append(c.coordinates)
+        c = cleanCoordinates(c.coordinates)
+        coords.append(c)
+
     return coords
 
 def getKML(url):
     r = requests.get(url)
     rawKML = r.content
-    root = parser.fromstring(rawKML)
-    coordsRaw = getCoordsFromRoot(root)
-    coordsClean = list()
+    return rawKML
+    
 
-    for c in coordsRaw:
-
-        # Clean up padding whitespace
-        c = re.sub('\s*', '', str(c))
-        coords = c.split(',')
-
-        # Remove leading zeroes from negative values, since float() fails on that format.
-        coords = [re.sub('^0-', '-', c) for c in coords]
-        # Convert all numbers to floats
-        coords = [float(c) for c in coords]
-
-        coordsClean.append(coords)
-
+    
+    coords = extractCoordinatesFromKML(rawKML)
     return coordsClean
 
 
