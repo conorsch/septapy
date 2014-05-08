f59edeb5b4e03fdbbee8e1ca1aac00a9bad56b64
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-08 12:50:40 -0400

    looks to be working

--- scratch.py
+++ scratch.py
@@ -3,45 +3,34 @@ import requests
 from pykml import parser
 import re
 
-def getCoordinatesNamespaces(root):
-    coordinatesNamespaces = []
-    dp = root.descendantpaths()
-    for d in dp:
-        if re.match(r'.*coordinates$', d):
-            print d
-            ns = re.sub('{http://www.opengis.net/kml/2.2}', '', d)
-            print ns
-            print type(ns)
-            coordinatesNamespaces.append(ns)
-
-    return coordinatesNamespaces
+def getCoordsFromRoot(root):
+    coords = list()
+    for c in root.Document.Placemark.MultiGeometry.getchildren():
+        coords.append(c.coordinates)
+    return coords
 
 def getKML(url):
     r = requests.get(url)
     rawKML = r.content
     root = parser.fromstring(rawKML)
+    coordsRaw = getCoordsFromRoot(root)
+    coordsClean = list()
 
-    tree = objectify.fromstring(rawKML)
-    coordinatesNamespaces = getCoordinatesNamespaces(root)
-    print "Found %s coordinate namespaces, and they are:" % len(coordinatesNamespaces)
+    for c in coordsRaw:
 
-    for c in coordinatesNamespaces:
-        try:
-            coords = getattr(root, c) 
+        # Clean up padding whitespace
+        c = re.sub('\s*', '', str(c))
+        coords = c.split(',')
 
-            # Clean up padding whitespace
-            c = re.sub('\s*', '', c)
+        # Remove leading zeroes from negative values, since float() fails on that format.
+        coords = [re.sub('^0-', '-', c) for c in coords]
+        # Convert all numbers to floats
+        coords = [float(c) for c in coords]
 
-            coords = c.split(',')
-            # Remove leading zeroes from negative values, since float() fails on that format.
-            coords = [re.sub('^0-', '-', c) for c in coords]
-            # Convert all numbers to floats
-            coords = [float(c) for c in coords]
+        coordsClean.append(coords)
 
-            return coords
+    return coordsClean
 
-        except AttributeError, e:
-            print e
 
 if __name__ == '__main__':
     getKML(tests.mockdata.KMLURL)
