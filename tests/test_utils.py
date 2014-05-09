93372fd912a38d0780821fd00ab55536333cd7ae
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-09 12:37:25 -0400

    more explicit imports in tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import unittest
-sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(__file__), '../septapy/') ))
+sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(os.path.dirname(__file__)), 'septapy') ))
 import septapy
 import mockdata
 
